7ee71476267b00bc921ce5b8398bf75d7d9d5b0e
Yuki Fujiwara <sky.y.0079@gmail.com>
2019-06-28 09:07:19 +0900

    藤原：初期状態

---
+++ 01.js
@@ -0,0 +1,10 @@
+// FizzBuzz
+
+// サンプル: "1" という文字列を出力
+console.log("1");
+
+// サンプル: "FizzBuzz" という文字列を出力
+// (process.stdout.write で改行なしの出力ができる)
+process.stdout.write("Fizz");
+process.stdout.write("Buzz");
+console.log();
